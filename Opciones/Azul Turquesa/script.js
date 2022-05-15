93809fa01f777ce2097a7a2e44b4e50ae57701a0
belenmoreno-github <80197429+belenmoreno-github@users.noreply.github.com>
2022-05-15 13:22:45 +0200

    nuevos commits

--- Opciones/Azul Turquesa/script.js
+++ Opciones/Azul Turquesa/script.js
@@ -6,16 +6,3 @@ $(document).ready(function() {
     });
   });
 
-  // simply preloader
-$('.tumb > img').each(function() {
-  $(this).css({
-    opacity: 0
-  }).load(function() {
-    $(this).animate({
-      opacity: 1
-    }, 1000);
-  }).attr('src', $(this).data('src'))
-  // wait and remove data-src
-  .delay(100)
-  .attr('data-src','');
-});
